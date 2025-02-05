70832732dc070a8512db7e3018d4ab0a8053a908
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2025-02-04 19:37:37 -0500

    rename totdeaths_orig for adhoc

--- src/combine-studies-adhoc/add-adhoc.R
+++ src/combine-studies-adhoc/add-adhoc.R
@@ -29,17 +29,27 @@ names(dat)[!(names(dat) %in% names(adhoc))]
 # Rename in adhoc to match studies
 adhoc <- adhoc %>%
   mutate(ref_id = study_id,
-         strata_id = paste0("Adhoc",study_id),
-         article_id = paste0("Adhoc",study_id),
+         strata_id = paste0("adHoc", study_id),
+         article_id = paste0("adHoc", study_id),
          location_long = NA,
          strata_other1 = NA,
-         strata_other2 = NA) %>%
+         strata_other2 = NA,
+         totdeaths = totdeaths_orig) %>%
   rename(va_alg = VA.algorithm,
          year_mid = yr_mid, 
          countryname = country,
          location_short = study_loc
   ) %>%
-  select(-c(study_id, totdeaths_orig))
+  select(-c(study_id, totdeaths_orig)) %>%
+  # Step 1: Calculate cod_n using cod_p (percentage) and totdeaths_orig
+  mutate(across(starts_with("cod_n"), ~ if_else(
+    # Condition: cod_n is NA, cod_p is not NA, and totdeaths_orig is not NA
+    is.na(.x) & !is.na(as.numeric(get(paste0("cod_p", sub("cod_n", "", cur_column()))))) & !is.na(as.numeric(totdeaths)),
+    # True: Calculate cod_n as (cod_p / 100) * totdeaths_orig (convert percentage to proportion)
+    (as.numeric(get(paste0("cod_p", sub("cod_n", "", cur_column())))) / 100) * as.numeric(totdeaths),
+    # False: Retain the original value of cod_n
+    .x
+  )))
 
 # Check that both datasets now have all the same columns
 names(adhoc)[!(names(adhoc) %in% names(dat))] # 0
