fc05aad23cb124b0a372a23ed22561cde6c198e7
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2025-02-11 13:39:05 -0500

    add new extracted vars for neonates

--- src/make.R
+++ src/make.R
@@ -58,7 +58,7 @@ if(ageSexSuffix %in% c("05to09y", "10to14y","15to19yF","15to19yM")){
 
 source("./src/update-old-studies/create-covar-key.R", local=new.env())
 #source("./src/update-old-studies/audit-covar.R", local=new.env())
-if(ageSexSuffix %in% c("01to59m")){
+if(ageSexSuffix %in% c("00to28d", "01to59m")){
   source("./src/update-old-studies/add-new-extracted-vars.R", local=new.env())
 }
 source("./src/update-old-studies/set-idvars.R", local=new.env())
