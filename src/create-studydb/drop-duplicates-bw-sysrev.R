04974410d30a5b9ee7fd351c91294b53a4b3e5e1
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2025-02-14 12:57:05 -0500

    added extra script to do quality checks and save final version of database

--- src/create-studydb/drop-duplicates-bw-sysrev.R
+++ src/create-studydb/drop-duplicates-bw-sysrev.R
@@ -127,10 +127,6 @@ dat <- dat %>%
 
 # Save outputs ------------------------------------------------------------
 
-write.csv(dat, paste0("./gen/create-studydb/output/StudyDatabase2023_",ageSexSuffix,"_",format(Sys.Date(), format="%Y%m%d"),".csv"), row.names = FALSE)
+write.csv(dat, paste0("./gen/create-studydb/temp/studies-combined_dup-drop_",ageSexSuffix,".csv"), row.names = FALSE)
 
-# Notes:
-# -CODs can have NA in them
-# -Some study data points are for the same population with different VA algorithms applied.
-# subset(dat, va_mult_ind == 1)
 
