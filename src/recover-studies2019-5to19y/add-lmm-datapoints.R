45b87ad69371edfb329642c8ed89ad0e5719da81
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2025-02-25 08:15:32 -0500

    adding deaths for lmm

--- src/recover-studies2019-5to19y/add-lmm-datapoints.R
+++ src/recover-studies2019-5to19y/add-lmm-datapoints.R
@@ -8,6 +8,7 @@ rm(list = ls())
 #' Libraries
 library(tidyverse)
 library(data.table)
+library(countrycode)
 #' Inputs
 source("./src/set-inputs.R")
 ## Old age-specific study database
@@ -17,49 +18,93 @@ if(ageSexSuffix == "05to09y"){load("./data/model-inputs-old/20201217-Data5to9-VA
 if(ageSexSuffix == "10to14y"){load("./data/model-inputs-old/20201222-Data10to14-VAMCM009-Test8j.RData")}
 if(ageSexSuffix == "15to19yF"){load("./data/model-inputs-old/20210207-Data15to19Fem-VAMCM009-Test9.RData")}
 if(ageSexSuffix == "15to19yM"){load("./data/model-inputs-old/20210212-Data15to19Men-VAMCM009-Test9e.RData")}
+## Key with cod reclassification
+dat_filename <- list.files("./data/classification-keys")
+dat_filename <- dat_filename[grepl("codreclassification", dat_filename, ignore.case = TRUE)]
+dat_filename <- dat_filename[grepl(ageSexSuffix, dat_filename)] 
+dat_filename <- tail(sort(dat_filename),1) # Most recent
+key_cod <- read.csv(paste0("./data/classification-keys/", dat_filename, sep = ""))
 ################################################################################
+
+# Reclassified CODs for this age group (includes Other and Undetermined)
+df_reclass <- subset(key_cod, !is.na(cod_reclass))
+# Exclude "TB" which has been redistributed (only present in 5-9y and 10-14y)
+df_reclass <- subset(df_reclass, cod_reclass != "TB")
+# Exclude "Undetermined" which is used to eliminate studies in cleaning phase
+df_reclass <- subset(df_reclass, cod_reclass != "Undetermined")
+df_reclass <- df_reclass[,c("cod_mapped", "cod_reclass")]
+v_cod_reclass <- unique(df_reclass$cod_reclass)
 
 # Find LMM data points that are in model input but not study database
 v_lmm <- studies$id[!(studies$id %in% studydb$id)]
-df_lmm <- subset(studies, id %in% v_lmm)
+df_lmm_id <- subset(studies, id %in% v_lmm)[,c("sid", "id", "reterm", "totdeaths")]
+df_lmm_dth <- subset(deaths, id %in% v_lmm)
+
+# Reshape deaths wide from cause column in model object
+# Make sure names match studydb
+df_lmm_dthWide <- df_lmm_dth %>% 
+  select(sid, id, cause, n) %>%
+  pivot_wider(
+    names_from = cause,
+    values_from = n
+  ) %>%
+  rename_with(
+    ~ case_when(
+      . == "RTA" ~ "RTI",
+      . == "Other_inj" ~ "OtherInj",
+      #. == "Interp_violence" ~ "InterpVio" , !!!! DO THIS HERE? OR IN HARMONIZE-COD?
+      #. == "Self_harm" ~ "SelfHarm",
+      TRUE ~ .)
+  )
+
+# Merge LMM covar and deaths
+df_lmm <- merge(df_lmm_id, df_lmm_dthWide, by = c("sid", "id"))
+if(nrow(df_lmm) != nrow(df_lmm_id)){
+  warning("Problem with LMM covar and eaths merge.")
+}
 
 if(ageSexSuffix %in% c("05to09y")){
-  df_lmm$sex <- substr(sexLabels[1],1,1)
   df_lmm$age_lb <- 5
   df_lmm$age_ub <- 9
 }
 if(ageSexSuffix %in% c("10to14y")){
-  df_lmm$sex <- substr(sexLabels[1],1,1)
   df_lmm$age_lb <- 10
   df_lmm$age_ub <- 14
 }
 if(ageSexSuffix %in% c("15to19yF")){
-  df_lmm$sex <- substr(sexLabels[2],1,1)
   df_lmm$age_lb <- 15
   df_lmm$age_ub <- 19
 }
 if(ageSexSuffix %in% c("15to19yM")){
-  df_lmm$sex <- substr(sexLabels[3],1,1)
   df_lmm$age_lb <- 15
   df_lmm$age_ub <- 19
 }
 
-# Add variables to match with study database
+# Add variables to match with old study database
 df_lmm$iso3 <- df_lmm$reterm
 df_lmm$year <- as.numeric(substr(df_lmm$id, 5, 8))
 df_lmm$Refid <- "LMM"
 df_lmm$sid <- paste0("VR", 1:nrow(df_lmm))
+df_lmm$whoname <- countrycode(df_lmm$iso3, origin = "iso3c", destination = "country.name")
 
 # Add location_fac value which is equivalent to "nationwide"
 # To be used in creation of informative id in /update-covar-for-old-studies/set-idvars
 df_lmm$location_fac <- 1
 
-# Delete all covariate columns. All will be replaced.
-df_lmm <- df_lmm[,c("sid","id","Refid","reterm","iso3","year","location_fac","totdeaths", "sex", "age_lb", "age_ub")]
-
 # Add to study database
 studydb$sid <- as.character(studydb$sid)
 dat <- dplyr::bind_rows(studydb, df_lmm)
+
+# Recode sex
+if(ageSexSuffix %in% c("05to09y", "10to14y")){
+  dat$sex <- sexLabels[1]
+}
+if(ageSexSuffix %in% c("15to19yF")){
+  dat$sex <- sexLabels[2]
+}
+if(ageSexSuffix %in% c("15to19yM")){
+  dat$sex <- sexLabels[3]
+}
 
 # Save output(s) ----------------------------------------------------------
 
