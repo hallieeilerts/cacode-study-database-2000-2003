5d7c7971392a0576bdeb1dfc99230e685a77daa3
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2025-02-25 08:13:54 -0500

    changing adhoc ref_id, strata_id, article_id

--- src/combine-studies-adhoc/add-adhoc.R
+++ src/combine-studies-adhoc/add-adhoc.R
@@ -27,10 +27,19 @@ names(dat)[!(names(dat) %in% names(adhoc))]
 # Create age group in months variable
 
 # Rename in adhoc to match studies
+# For ref_id, remove the last underscore which is effectively the strata number
 adhoc <- adhoc %>%
-  mutate(ref_id = study_id,
-         strata_id = paste0("adHoc", study_id),
-         article_id = paste0("adHoc", study_id),
+  # if has underscore and trailing numbers, remove all
+  # if does not have underscore, remove numbers at end of string
+  # if has lower-case letter at end of string, remove last three characters
+  mutate(hasUnderscoreNum = grepl("_[0-9]+$", study_id),
+         article_id = sub("_(\\d+)$", "", study_id),
+         hasNum = grepl("[0-9]+$", study_id),
+         article_id = ifelse(hasUnderscoreNum == FALSE & hasNum == TRUE, sub("[0-9]+$", "", study_id), article_id),
+         article_id = ifelse(hasUnderscoreNum == FALSE & hasNum == FALSE & grepl("[a-z]$", study_id), 
+                         substr(study_id, 1, nchar(study_id) - 3), article_id)) %>%
+  mutate(strata_id = study_id,
+         ref_id = "AdHoc",
          location_long = NA,
          strata_other1 = NA,
          strata_other2 = NA,
@@ -42,7 +51,7 @@ adhoc <- adhoc %>%
          countryname = country,
          location_short = study_loc
   ) %>%
-  select(-c(study_id, totdeaths_orig)) %>%
+  select(-c(study_id, totdeaths_orig, hasUnderscoreNum, hasNum)) %>%
   # Step 1: Calculate cod_n using cod_p (percentage) and totdeaths_orig
   mutate(across(starts_with("cod_n"), ~ if_else(
     # Condition: cod_n is NA, cod_p is not NA, and totdeaths_orig is not NA
@@ -51,7 +60,7 @@ adhoc <- adhoc %>%
     (as.numeric(get(paste0("cod_p", sub("cod_n", "", cur_column())))) / 100) * as.numeric(totdeaths),
     # False: Retain the original value of cod_n
     .x
-  )))
+  ))) 
 
 # Check that both datasets now have all the same columns
 names(adhoc)[!(names(adhoc) %in% names(dat))] # 0
