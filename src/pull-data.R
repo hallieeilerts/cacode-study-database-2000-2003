bcc1670fa2213d41cf3d4817a9006fe2e00f60e2
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2025-02-28 11:16:49 -0500

    changes in data warehouse folder names

--- src/pull-data.R
+++ src/pull-data.R
@@ -14,97 +14,97 @@ source("./src/fn_initEnvironmentData.R")
 
 # Prediction database
 fn_initEnvironmentData("prediction-database")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/databases/prediction-database", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/processed-data/prediction-database", sep = ""))
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/databases/prediction-database/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/processed-data/prediction-database/",dat_filename[i], sep = ""),
             to   = paste0("./data/prediction-database/",dat_filename[i]))
 }
 
 # India subnational prediction database
 fn_initEnvironmentData("prediction-database-india")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2021/databases/prediction-database-india", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2021/processed-data/prediction-database-india", sep = ""))
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2021/databases/prediction-database-india/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2021/processed-data/prediction-database-india/",dat_filename[i], sep = ""),
             to   = paste0("./data/prediction-database-india/",dat_filename[i]))
 }
 
 # Old study data
 fn_initEnvironmentData("study-data-old")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2021/databases/study-database", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2021/processed-data/study-database", sep = ""))
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2021/databases/study-database/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2021/processed-data/study-database/",dat_filename[i], sep = ""),
             to   = paste0("./data/study-data-old/",dat_filename[i]))
 }
 
 # Old study data with new variables extracted (VA alogirthms, age bounds)
 fn_initEnvironmentData("study-data-old-augmented")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/databases/study-data-old-augmented", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/processed-data/study-database-supplements/reextracted-var-2000-2019-studies", sep = ""))
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/databases/study-data-old-augmented/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/processed-data/study-database-supplements/reextracted-var-2000-2019-studies/",dat_filename[i], sep = ""),
             to   = paste0("./data/study-data-old-augmented/",dat_filename[i]))
 }
 
 # Old model inputs
 fn_initEnvironmentData("model-inputs-old")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2021/data/model-inputs", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2021/processed-data/model-inputs", sep = ""))
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2021/data/model-inputs/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2021/processed-data/model-inputs/",dat_filename[i], sep = ""),
             to   = paste0("./data/model-inputs-old/",dat_filename[i]))
 }
 
 # New study data
 fn_initEnvironmentData("study-data")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/data/study-data", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/original-data/study-data", sep = ""))
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/data/study-data/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/original-data/study-data/",dat_filename[i], sep = ""),
             to   = paste0("./data/study-data/",dat_filename[i]))
 }
 
 # Ad hoc data
 fn_initEnvironmentData("ad-hoc")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/databases/ad-hoc", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/processed-data/study-database-supplements/ad-hoc", sep = ""))
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/databases/ad-hoc/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/processed-data/study-database-supplements/ad-hoc/",dat_filename[i], sep = ""),
             to   = paste0("./data/ad-hoc/",dat_filename[i]))
 }
 
 # Calibrated study data
 fn_initEnvironmentData("study-data-calibrated")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/databases/study-data-calibrated", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/processed-data/study-database-supplements/calibration", sep = ""))
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/databases/study-data-calibrated/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/processed-data/study-database-supplements/calibration/",dat_filename[i], sep = ""),
             to   = paste0("./data/study-data-calibrated/",dat_filename[i]))
 }
 
 # Single causes database
 fn_initEnvironmentData("single-causes")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/databases/single-causes", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/processed-data/single-causes", sep = ""))
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/databases/single-causes/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/processed-data/single-causes/",dat_filename[i], sep = ""),
             to   = paste0("./data/single-causes/",dat_filename[i]))
 }
 
 # Classification keys
 fn_initEnvironmentData("classification-keys")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/data/classification-keys", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/keys", sep = ""))
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/data/classification-keys/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/keys/",dat_filename[i], sep = ""),
             to   = paste0("./data/classification-keys/",dat_filename[i]))
 }
 
 # Covariate data extraction from DHS
 fn_initEnvironmentData("dhs")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/databases/dhs-covariate-extraction", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/processed-data/dhs-covariate-extraction", sep = ""))
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/databases/dhs-covariate-extraction/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/processed-data/dhs-covariate-extraction/",dat_filename[i], sep = ""),
             to   = paste0("./data/dhs/",dat_filename[i]))
 }
 
 # pfpr covariate data
 fn_initEnvironmentData("study-pfpr")
-dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/data/study-pfpr", sep = ""))
+dat_filename <- list.files(paste0(pathDataWarehouse, "/2000-2023/original-data/study-pfpr", sep = ""))
 dat_filename <- dat_filename[grepl("pfpr_map", dat_filename, ignore.case = TRUE)]
 for(i in 1:length(dat_filename)){
-  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/data/study-pfpr/",dat_filename[i], sep = ""),
+  file.copy(from = paste0(pathDataWarehouse, "/2000-2023/original-data/study-pfpr/",dat_filename[i], sep = ""),
             to   = paste0("./data/study-pfpr/",dat_filename[i]))
 }
