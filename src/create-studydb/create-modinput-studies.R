8d0aaf2f878a8161263030b903d16bae3774fa99
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2025-05-13 10:31:16 -0400

    deleting fn_setCov, loading covariates via cov key spreadsheet

--- src/create-studydb/create-modinput-studies.R
+++ src/create-studydb/create-modinput-studies.R
@@ -7,7 +7,6 @@ rm(list = ls())
 #' Libraries
 #' Inputs
 source("./src/set-inputs.R")
-source("./src/create-studydb/fn_setCov.R")
 ## Combined old and new studies
 dat_filename <- list.files("./gen/create-studydb/output")
 dat_filename <- dat_filename[grepl("StudyDatabase2023", dat_filename, ignore.case = TRUE)]
@@ -15,17 +14,22 @@ dat_filename <- dat_filename[grepl(ageSexSuffix, dat_filename)]
 dat_filename <- dat_filename[!grepl("Codebook", dat_filename, ignore.case = TRUE)]
 dat_filename <- tail(sort(dat_filename), 1)
 dat <- read.csv(paste0("./gen/create-studydb/output/", dat_filename, sep = ""))
+## Model covariate list
+dat_filename <- list.files("./data/classification-keys")
+dat_filename <- dat_filename[grepl("CovariateDatabase2023_ModelCovariateList", dat_filename, ignore.case = TRUE)]
+dat_filename <- tail(sort(dat_filename), 1)
+df_covar <- readxl::read_excel(paste0("./data/classification-keys/",dat_filename), sheet = "model-covar-long")
 ################################################################################
 
 # limited id variables for model input
 v_idvars <- c("recnr","id", "reterm", "totdeaths")
 
 # Load covariate names
-vxf <- fn_setCov(ageSexSuffix)
+vxf <- subset(df_covar, Model == "HMM" & AgeSexSuffix == ageSexSuffix)$Covariate
 
 studies <- dat[, c(v_idvars, vxf)]
 rownames(studies) <- NULL
 
 # Save output(s) ----------------------------------------------------------
 
-save(studies, file = paste("./gen/create-studydb/output/ModInput2023-Studies_",ageSexSuffix,"_",resDate,".RData", sep = ""))
+save(studies, file = paste("./gen/create-studydb/output/ModInput2023-HMM-Studies_",ageSexSuffix,"_",resDate,".RData", sep = ""))
